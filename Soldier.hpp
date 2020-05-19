181a6a1f8b0651d7fb05cd3324e17ba44462181f
Omer <omer2katz@gmail.com>
2020-05-19 14:22:23 +0300

    Make peace not war

---
+++ Soldier.hpp
@@ -0,0 +1,11 @@
+namespace WarGame {
+    class Soldier{
+        private:
+        int hp; // health points
+        int power; //heal or damage
+
+move(); //change pos in the board
+act();//git or heal, depends on the soldier type
+
+    }
+}
